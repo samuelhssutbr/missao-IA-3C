8e71b18126bcf9c174efbf6cbd9b0135cba258de
samuelhssutbr <161604388+samuelhssutbr@users.noreply.github.com>
2024-09-02 10:22:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,67 +8,6 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const jogarnovamente = document.querySelector(".novamente-btn");
-const perguntas = [
-    {
-        enunciado: "Pergunta 1",
-        alternativas: [
-            {
-                texto: "Alternativa 1 da pergunta 1",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Alternativa 2 da pergunta 1",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }           
-            
-        ]
-    },
-    {
-        enunciado: "Pergunta 2",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Alternativa 2 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-        ]
-    },
-    {
-        enunciado: "Pergunta 3",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto:"Alternativa 2 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-            
-        ]
-    },
-];
 
 let atual = 0; 
 let perguntaAtual;
