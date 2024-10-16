3b880006139227e05130b42ca46f26b9a475b386
samuelhssutbr <161604388+samuelhssutbr@users.noreply.github.com>
2024-10-16 07:38:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,23 +7,24 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn"); 
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
 
 let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
 
-botãoIniciar.addEventListener('click', iniciaJogo);
+botaoIniciar.addEventListener('click', iniciaJogo);
 
 function iniciaJogo() {
-    atual = "";
-    historiaFinal= "";
-    telaInicial.style.display = none;
-    caixaPerguntas.classList.remove("mostrar)"
-    caixaAlternativas.classlist.remove("mostrar");
-    caixaResultado.clssList.remove("mostrar");
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
     mostraPergunta();
 }
-
 
 function mostraPergunta() {
     if(atual >= perguntas.length){
@@ -48,28 +49,33 @@ function mostraAlternativas(){
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
-    atual++;
+   if(opcaoSelecionada.proxima !== undefined) {
+       atual = opcaoSelecionada.proxima;
+   }else {
+       mostraResultado();
+       return;
+   }
     mostraPergunta();
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Após os estudos, ${nome} descobriu que`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
-    caixaResultado.classList.add("mostrar"); /*add*/
+    caixaResultado.classList.add("mostrar"); 
     botaoJogarNovamente.addEventListener("click", jogarNovamente); 
 }
 
 function jogarNovamente(){
     atual = 0;
     historiaFinal = "";
-    caixaResultado.classList.remove("mostrar"); /*add*/
+    caixaResultado.classList.remove("mostrar"); 
     mostraPergunta();
 }
-    
+
 function substituiNome() {
     for(const pergunta of perguntas) {
-        pergunta.enunciado = pergunta.enunciado.replece(\voce\g, nome);
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
     }
 }
 substituiNome();
